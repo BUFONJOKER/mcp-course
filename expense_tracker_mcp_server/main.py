ea7f9d925e744089ed806533be3235b5424b64ce
Abdul Rehman Javaid <134438770+BUFONJOKER@users.noreply.github.com>
2026-03-11 03:17:32 +0500

    6 videos completed

--- expense_tracker_mcp_server/main.py
+++ expense_tracker_mcp_server/main.py
@@ -1,12 +1,15 @@
 from fastmcp import FastMCP
 import os
 import sqlite3
+import json
 
 mcp = FastMCP(name='Expense Tracker MCP Server')
 
 # This ensures the DB is always created in the same folder as main.py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "expenses.db")
+
+CATEGORIES_JSON_PATH = os.path.join(BASE_DIR, "categories.json")
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
@@ -53,6 +56,39 @@ def add_expense(date: str, amount: float, category: str, subcategory: str = ''):
         ''', (date, amount, category, subcategory))
         conn.commit()
     return "Expense added successfully."
+
+@mcp.tool
+def summarize(start_date: str, end_date: str, category: str = ''):
+    """Summarizes expenses by category if category given otherwise summarize all expenses."""
+    with get_connection() as conn:
+        if category!='':
+            cursor = conn.execute('''
+                SELECT category, SUM(amount) as total
+                FROM expenses
+                WHERE date BETWEEN ? AND ? AND category = ?
+                GROUP BY category
+            ''', (start_date, end_date, category))
+        else:
+            cursor = conn.execute('''
+                SELECT category, SUM(amount) as total
+                FROM expenses
+                WHERE date BETWEEN ? AND ?
+                GROUP BY category
+            ''', (start_date, end_date))
+        summary = cursor.fetchall()
+    return summary
+
+@mcp.resource("expense://categories", mime_type="application/json")
+def categories():
+    """Returns the list of categories from the JSON file."""
+
+    try:
+        with open(CATEGORIES_JSON_PATH, 'r', encoding="utf-8") as f:
+            data = f.read()
+        return data
+    except Exception as e:
+        return json.dumps({"error": str(e)})
+
 
 if __name__ == "__main__":
     mcp.run()
